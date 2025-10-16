98470db3f37561bdb1c1d1e41f6d7cb6fea29901
Vladislav Gubarev <gubarev@api4.ai>
2025-10-16 18:04:27 +0300

    Updated python samples: Added "normal" mode

--- python/requests/sample.py
+++ python/requests/sample.py
@@ -8,15 +8,17 @@ import sys
 import requests
 
 
-# Use 'demo' mode just to try api4ai for free. Free demo is rate limited.
-# For more details visit:
-#   https://api4.ai
+# Use 'demo' mode just to try api4ai for free. ⚠️ Free demo is rate limited and must not be used in real projects.
+#
+# Use 'normal' mode if you have an API Key from the API4AI Developer Portal. This is the method that users should normally prefer.
 #
 # Use 'rapidapi' if you want to try api4ai via RapidAPI marketplace.
 # For more details visit:
 #   https://rapidapi.com/api4ai-api4ai-default/api/alcohol-label-recognition/details
 MODE = 'demo'
 
+# Your API4AI key. Fill this variable with the proper value if you have one.
+API4AI_KEY = ''
 
 # Your RapidAPI key. Fill this variable with the proper value if you want
 # to try api4ai via RapidAPI marketplace.
@@ -25,8 +27,11 @@ RAPIDAPI_KEY = ''
 
 OPTIONS = {
     'demo': {
-        'url': 'https://demo.api4ai.cloud/alco-rec/v1/results',
-        'headers': {'A4A-CLIENT-APP-ID': 'sample'}
+        'url': 'https://demo.api4ai.cloud/alco-rec/v1/results'
+    },
+    'normal': {
+        'url': 'https://api4ai.cloud/alco-rec/v1/results',
+        'headers': {'X-API-KEY': API4AI_KEY}
     },
     'rapidapi': {
         'url': 'https://alcohol-label-recognition.p.rapidapi.com/v1/results',
@@ -40,20 +45,20 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         image = sys.argv[1]
     else:
-        image = 'https://storage.googleapis.com/api4ai-static/samples/alco-rec-1.jpg'
+        image = 'https://static.api4.ai/samples/alco-rec-1.jpg'
 
     if '://' in image:
         # POST image via URL.
         response = requests.post(
             OPTIONS[MODE]['url'],
-            headers=OPTIONS[MODE]['headers'],
+            headers=OPTIONS[MODE].get('headers'),
             data={'url': image})
     else:
         # POST image as file.
         with open(image, 'rb') as image_file:
             response = requests.post(
                 OPTIONS[MODE]['url'],
-                headers=OPTIONS[MODE]['headers'],
+                headers=OPTIONS[MODE].get('headers'),
                 files={'image': (os.path.basename(image), image_file)}
             )
 
